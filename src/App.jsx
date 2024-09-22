cf7383f9171f4319db333cf804e210f28426c4e0
tanri-tiw <pranjaltiwari291@gmail.com>
2024-09-23 00:12:31 +0530

    4

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,7 @@ import "./App.css";
 import Port from "./components/Port";
 import About from "./Pages/About";
 const App = () => {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Port />} />
-        <Route path="/about" element={<About />} />
-      </Routes>
-    </BrowserRouter>
-  );
+  return <Port />;
 };
 
 export default App;
